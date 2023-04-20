f45e4d344a5e2fefe01758c43c47194d9f2ba437
baranburak19 <baranburak12@hotmail.com>
2023-04-20 17:54:36 +0300

    Backend controllers small change

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -40,15 +40,13 @@ const updateGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal id not found')
     }
 
-    const user = await User.findById(req.user.id)
-
-    if(!user) {
+    if(!req.user) {
         res.status(401)
         throw new Error('User not found')
     }
 
     // Make sure logged in user matches the goal's user
-    if(goal.user.toString() != user.id) {
+    if(goal.user.toString() != req.user.id) {
         res.status(401)
         throw new Error('User not authorized')
     }
@@ -69,15 +67,13 @@ const deleteGoal = asyncHandler(async (req, res) => {
         throw new Error('Goal id not found')
     }
 
-    const user = await User.findById(req.user.id)
-
-    if(!user) {
+    if(!req.user) {
         res.status(401)
         throw new Error('User not found')
     }
 
     // Make sure logged in user matches the goal's user
-    if(goal.user.toString() != user.id) {
+    if(goal.user.toString() != req.user.id) {
         res.status(401)
         throw new Error('User not authorized')
     }
